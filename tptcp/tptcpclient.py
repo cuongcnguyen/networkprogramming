1c014f9438bb3eab3e2d63d1fc9d87f7a7849bcf
cuong <cuongcao911@gmail.com>
2019-04-28 00:11:52 +0300

    Created udp client and server

--- tptcp/tptcpclient.py
+++ tptcp/tptcpclient.py
@@ -1,12 +1,16 @@
 import socket
 import datetime, time
+from struct import unpack
+from print_time import print_time
 
 mysocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(("127.0.0.1", 37))
 data = mysocket.recv(2048)
 data= int.from_bytes(data, byteorder="big")
 
+mysocket.send(b"Connected to the client. Data sent.")
+mysocket.close()
+
 print (data)
 
-mysocket.send(b"Connected to the client. Connection established")
-mysocket.close()
+print_time(data)
